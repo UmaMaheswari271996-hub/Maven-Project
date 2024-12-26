a56507049b16b4e7ca0174dcd9066d92fa26764c
UMA <UMA@DESKTOP-LSMVHLH>
2024-12-26 09:14:52 +0530

    First commit

---
+++ mavenn/src/test/java/com/crm/testscript/AccountsModule.java
@@ -0,0 +1,11 @@
+package com.crm.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AccountsModule {
+	@Test
+public void demo() {
+	Reporter.log("demo",true);
+}
+}
